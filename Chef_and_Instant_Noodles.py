7cd2fd9365a6fbd9401800097e2cfdf972a1e162
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2024-12-01 22:00:08 +0530

    Create Chef_and_Instant_Noodles.py

---
+++ Chef_and_Instant_Noodles.py
@@ -0,0 +1,9 @@
+X, Y = map(int, input().split())
+result = X * Y
+print(result)
+
+// we can also do it some other way-
+X =int(input("Enter the number of stoves: "))
+Y =int(input("Enter the number of minutes: "))
+Z = X * Y
+print("the total no:" , Z)
