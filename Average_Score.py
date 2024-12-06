6330c72c30809610f4992688d260ecc4d91fcea7
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2024-12-07 00:32:51 +0530

    Update Average_Score.py

--- Average_Score.py
+++ Average_Score.py
@@ -1,6 +1,8 @@
-marks1 = 95
-marks2 = 80.5
-marks3 = 58.5 
-N = 3 
-avg=(marks1 + marks2 + marks3)/N
-print(avg)
+scores = input()
+
+X, Y, Z = map(float, scores.split())
+
+average = (X + Y + Z) / 3
+
+print(round(average, 1))
+
