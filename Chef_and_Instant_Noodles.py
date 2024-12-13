629ad9fbae0ce17766717866d9ec30e05f58b3c9
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2024-12-13 23:29:23 +0530

    Update Chef_and_Instant_Noodles.py

--- Chef_and_Instant_Noodles.py
+++ Chef_and_Instant_Noodles.py
@@ -5,5 +5,6 @@ print(result)
 // we can also do it some other way-
 X =int(input("Enter the number of stoves: "))
 Y =int(input("Enter the number of minutes: "))
+
 Z = X * Y
 print("the total no:" , Z)
