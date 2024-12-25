21bfe8553484ceda787fd8bbbd5b737b551da4a7
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2024-12-25 23:30:25 +0530

    Update Chef_and_Instant_Noodles.py

--- Chef_and_Instant_Noodles.py
+++ Chef_and_Instant_Noodles.py
@@ -2,6 +2,7 @@ X, Y = map(int, input().split())
 result = X * Y
 print(result)
 
+
 // we can also do it some other way-
 X =int(input("Enter the number of stoves: "))
 Y =int(input("Enter the number of minutes: "))
